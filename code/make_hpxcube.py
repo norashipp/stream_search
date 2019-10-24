506289631db87d78f0429201719d8ff2c281732b
Nora Shipp <nora@Noras-MacBook-Pro.local>
2019-10-24 17:09:44 -0400

    args

--- code/make_hpxcube.py
+++ code/make_hpxcube.py
@@ -88,8 +88,8 @@ if __name__ == '__main__':
                        survey]['moduli'][1] + dmu, dmu)
     # moduli = [15, 16]
     print('Moduli: ', moduli)
-    age = args.age  # 12.0  # from DES search, compared to 12.5, 0.0001, doesn't make much difference along main sequence
-    z = args.metallicity  # 0.0002
+    age = float(args.age)  # 12.0  # from DES search, compared to 12.5, 0.0001, doesn't make much difference along main sequence
+    z = float(args.metallicity)  # 0.0002
 
     metal_poor = False
     ###################
