e6dafd4a1b87caeead5517cda0bc8e051ce67927
Nora Shipp <nora@Noras-MacBook-Pro.local>
2020-02-21 16:23:24 -0500

    radius

--- code/select_object.py
+++ code/select_object.py
@@ -12,20 +12,20 @@ from rotation_matrix import phi12_rotmat
 import galstreams
 
 
-def get_object(ra, dec, survey='DECaLS', outfile='cutout.fits'):
+def get_object(ra, dec, survey='DECaLS', outfile='cutout.fits', radius=0.1):
     if survey == 'DECaLS':
         filename = '/data/des40.b/data/decals/dr8/south_skim/decals-dr8-sweep_%0.5d.fits'
     elif survey == 'BASS':
         filename = '/data/des40.b/data/decals/dr8/north_skim/decals-dr8-sweep_%0.5d.fits'
 
-    filenames = [filename % i for i in healpix.ang2disc(32, ra, dec, 5)]
+    filenames = [filename % i for i in healpix.ang2disc(32, ra, dec, np.maximum(5, radius * 2))]
     columns = ['RA', 'DEC', 'MAG_SFD_G', 'MAG_SFD_R', 'MAG_SFD_Z', 'EXTENDED_CLASS']
 
     data = load_infiles(filenames, columns=columns, multiproc=32)
 
     sel = (data['EXTENDED_CLASS'] == 0) & (data['MAG_SFD_G'] < 23.5) & (data['MAG_SFD_G'] > 16) & (data['MAG_SFD_G'] - data['MAG_SFD_R'] > 0) & (data['MAG_SFD_G'] - data['MAG_SFD_R'] < 1)
     sep = angsep(ra, dec, data['RA'], data['DEC'])
-    sel &= (sep < 0.1)
+    sel &= (sep < radius)
 
     tab = table.Table(data[sel])
     tab.write(outfile)
