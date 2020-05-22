a5e6c66e8add3d82892dc31305aed32a54861aff
Nora Shipp <nora@Noras-MBP.hsd1.il.comcast.net>
2020-05-22 10:54:32 -0500

    y6 data

--- code/surveys.py
+++ code/surveys.py
@@ -32,10 +32,12 @@ surveys = odict([
      dict(
          mag='SOF_PSF_MAG_CORRECTED_%s',
          ext=None,
-         data_dir='/data/des81.b/data/tavangar/skim_1.1/',
+         data_dir='/data/des81.b/data/mmcnanna/y6a1/skim_y6_gold_1_1/',
          fracdet=None,
          minmag=16.,
          maxmag=24.0,
+         stargal='EXT_SOF',
+         stargal_cut=1,
          err=lambda x: 0.0010908679647672335 + np.exp((x - 27.091072029215375) / 1.0904624484538419), # Y3
          C=[0.05, 0.1],
          E=2.,
