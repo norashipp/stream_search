73d20784497b1c9a727f456d8e5f4ebf0dedda62
Nora Shipp <norashipp@gmail.com>
2022-02-28 11:14:39 -0500

    fit photo errors

--- code/fit_photo_errors.py
+++ code/fit_photo_errors.py
@@ -1,6 +1,8 @@
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
+from astopy.io import fits as fitsio
 
 from scipy.stats import binned_statistic
 from scipy.optimize import curve_fit
